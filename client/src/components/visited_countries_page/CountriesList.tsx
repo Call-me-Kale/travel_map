a112ff5c06231fd719506e297e04af950ac70e4f
KaleIsProgramming <95875856+KaleIsProgramming@users.noreply.github.com>
2024-11-17 12:36:58 +0100

    Feat: Added basic styling into Visited Countries Page
    Feat: Added basic styling into Visited Countries Page

--- client/src/components/visited_countries_page/CountriesList.tsx
+++ client/src/components/visited_countries_page/CountriesList.tsx
@@ -1,12 +1,98 @@
 import styled from "@emotion/styled";
 import { Country } from "./Country";
 
+export interface Data {
+    name: string;
+    img: string;
+    description: string;
+}
 
 export const CountriesList = () => {
-
+    const data: Data[] = [
+        {
+            name: 'Poland',
+            img: 'poland.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Italy',
+            img: 'italy.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+            name: 'Japan',
+            img: 'japan.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+    ]
     return (
-        <>
-            <></>
-        </>
+        <StyledCountryList>
+            <Header>
+                <Text>Twoje podróże</Text>
+            </Header>
+            <List>
+                {data.map((country, i) => {
+                    return <Country data={country} key={i}/>
+                })}
+            </List>
+        </StyledCountryList>
     );
 };
+
+const StyledCountryList = styled.div`
+    height: 100%;
+    width: 100%;  
+    overflow-x: hidden;
+    overflow-y: hidden;
+`;
+
+const List = styled.div`
+    height: calc(100% - 140px);
+    width: 100%;  
+    display: flex;
+    flex-wrap: wrap;
+    overflow-x: hidden;
+    overflow-y: auto;
+    justify-content: center;
+`;
+const Header = styled.div`
+    height: 140px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Text = styled.h2`
+    font-size: 220%;
+`;
--- client/src/components/visited_countries_page/Country.tsx
+++ client/src/components/visited_countries_page/Country.tsx
@@ -1,11 +1,66 @@
+import { FC } from "react";
+import { Data } from "./CountriesList";
 import styled from "@emotion/styled";
 
+interface DataI {
+    data: Data
+}
 
-export const Country = () => {
-
+export const Country:FC<DataI> = ({data}) => {
+    const {name, img, description} = data;
     return (
-        <>
-            <></>
-        </>
+        <StyledCountry>
+            <ImageContainer></ImageContainer>
+            <CountryNameContainer>{name}</CountryNameContainer>
+            <DescriptionContainer><p>{description}</p></DescriptionContainer>
+        </StyledCountry>
     );
 };
+
+const StyledCountry = styled.div`
+    height: 400px;
+    width: 300px;
+    background: gray;
+    margin: 15px 20px;
+    border-radius: 20px;
+    cursor: pointer;
+`;
+
+const ImageContainer = styled.div`
+    height: 50%;
+    width: 100%;
+    background: black;  
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+`;
+
+const CountryNameContainer = styled.h3`
+    height: 10%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;  
+`;
+
+const DescriptionContainer = styled.div`
+    height: 40%;
+    width: 100%;
+    background: yellow;  
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+
+    p {
+        height: 80%;
+        width: 80%;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-line-clamp: 5; 
+                line-clamp: 5;
+        -webkit-box-orient: vertical;
+    }
+`;
